794524f27c63b40a656d229033b90ea7671be041
Victor Oviedo <drvictoroviedo@gmail.com>
2026-03-24 12:04:05 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const current = window.location.pathname.split('/').pop() || 'index.html';
+document.querySelectorAll('[data-nav]').forEach(link => {
+  const href = link.getAttribute('href');
+  if (href === current || (current === '' && href === 'index.html')) {
+    link.classList.add('active');
+  }
+});
